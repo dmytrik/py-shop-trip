5856e92e7129990445efba71ebce422b799b365f
dmytro <dmysamo@gmail.com>
2024-10-10 00:44:46 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
-def shop_trip():
-    # write your code here
-    pass
+from app.service import (
+    FUEL_PRICE,
+    customers,
+    shops,
+    show_the_cost_of_purchases,
+    take_checks
+)
+
+
+def shop_trip() -> None:
+    for customer in customers:
+        customer.print_info()
+        show_the_cost_of_purchases(customer, shops, FUEL_PRICE)
+        take_checks(customer, shops)
+        customer.back_home(shops, FUEL_PRICE)
